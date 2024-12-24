ebb63477ac877def35b2e313fecded6f3ac44d10
Zaira <zaira.gati@gmail.com>
2024-12-24 13:17:26 +0100

    Major overhaul: Updated HTML, CSS, and JavaScript files for improved functionality and design. Fixed bugs, added new features, and enhanced user experience.

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const TelegramBot = require('node-telegram-bot-api');
+const cors = require('cors'); // CORS hinzufügen
 
 const app = express();
+app.use(cors()); // CORS-Middleware verwenden
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Statische Dateien aus dem "public"-Verzeichnis bereitstellen
 app.use(express.static(path.join(__dirname, 'public')));
 
-const token = '7983061576:AAGKBvY5MxCTHnlgpgRZ97sBMqMfV1bVFAg';
+const token = '7983061576:AaAGKBvY5MxCTHnlgpgRZ97sBMqMfV1bVFAg';
 const chatId = '1460847056';
 const bot = new TelegramBot(token, { polling: true });
 
@@ -19,6 +21,7 @@ app.get('/test', (req, res) => {
 
 app.post('/submit', (req, res) => {
     const answers = req.body;
+    console.log('Empfangene Antworten:', answers); // Log hinzugefügt
     const message = `Antworten erhalten:\nFrage 1: ${answers.question1}\nFrage 2: ${answers.question2}`;
     
     bot.sendMessage(chatId, message)
@@ -29,7 +32,7 @@ app.post('/submit', (req, res) => {
         });
 });
 
-const PORT = 3000;
+const PORT = 4000; // Port sicherstellen
 app.listen(PORT, () => {
     console.log(`Server läuft auf Port ${PORT}`);
 });
